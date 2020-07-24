50a93aa37abe7c7c682d9dfc8399debc8c640d2c
Tran Minh Quang <quangtmps12135@fpt.edu.vn>
2020-07-24 11:22:38 -0700

    make cart page

--- server.js
+++ server.js
@@ -6,17 +6,25 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
 // Asset
 app.use(express.static('public'));
+
+
 // Set template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
