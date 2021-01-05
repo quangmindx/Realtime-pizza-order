f669cf897caa69588f50694ec6b2164fa360f3e5
quangmindx <quangtmps12135@fpt.edu.vn>
2021-01-05 17:47:31 +0700

    fix favicon, title...

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -3,8 +3,10 @@ function homeController() {
   return {
     async index(req, res) {
       const pizzas = await Menu.find();
-      console.log(pizzas);
-      return res.render("home", { pizzas: pizzas });
+      return res.render("home", {
+        title: "Realtime Pizza App",
+        pizzas: pizzas,
+      });
     },
   };
 }
